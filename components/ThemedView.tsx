bf34f989bab0332c4fa37f06a7fd5c6f597ef4c5
Angel Marfil <angelmarfil11@gmail.com>
2024-06-06 11:46:10 -0500

    first commit

--- components/ThemedView.tsx
+++ components/ThemedView.tsx
@@ -1,14 +1,10 @@
-import { View, type ViewProps } from 'react-native';
+import { Button } from "react-native";
 
-import { useThemeColor } from '@/hooks/useThemeColor';
-
-export type ThemedViewProps = ViewProps & {
-  lightColor?: string;
-  darkColor?: string;
-};
-
-export function ThemedView({ style, lightColor, darkColor, ...otherProps }: ThemedViewProps) {
-  const backgroundColor = useThemeColor({ light: lightColor, dark: darkColor }, 'background');
-
-  return <View style={[{ backgroundColor }, style]} {...otherProps} />;
+export function ThemedView({ navigation }: any) {
+  return (
+    <Button
+      title={"Holaaa"}
+      onPress={() => navigation.navigate("Screen2")}
+    ></Button>
+  );
 }
